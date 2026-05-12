9d1e94046b7648945a15d819d145db359d1abf2c
ElenaVlasenko <radionovaelena124@gmail.com>
2026-05-12 20:07:18 +0300

    add context privider

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,27 +1,26 @@
-import { CSSProperties } from 'react';
+import { CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from '../article/Article';
 import { ArticleParamsForm } from '../article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './../../constants/articleProps';
-
 import styles from './app.module.scss';
+import {
+	ArticleStylesContext,
+	defaultStyles,
+	StylesMap,
+} from 'src/context/articleStyles';
 
 export const App = () => {
+	const [articleStyles, setArticleStyles] = useState<StylesMap>(defaultStyles);
+
 	return (
-		<main
-			className={clsx(styles.main)}
-			style={
-				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
-				} as CSSProperties
-			}>
-			<ArticleParamsForm />
-			<Article />
-		</main>
+		<ArticleStylesContext.Provider value={{ articleStyles, setArticleStyles }}>
+			<main
+				className={clsx(styles.main)}
+				style={articleStyles as CSSProperties}>
+				<ArticleParamsForm />
+				<Article />
+			</main>
+		</ArticleStylesContext.Provider>
 	);
 };
